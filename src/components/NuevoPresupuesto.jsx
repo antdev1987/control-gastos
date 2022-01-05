360d9527f0f5b98f23e94b8d3e9c3148e5e4ee08
antdev1987 <antdev1987@gmail.com>
2022-01-04 23:51:45 -0600

    95 validando el presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,12 +1,29 @@
+import {useState} from 'react'
 
 import React from 'react'
+import Mensaje from './Mensaje'
+
+
+
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+   const [mensaje, setMensaje] =useState('')
+
+   const handlePresupuesto =(e)=>{
+      e.preventDefault()
+
+      if(!Number(presupuesto) || Number(presupuesto) < 0){
+         setMensaje('no es un presupuesto valido')
+      }else{
+         setMensaje('presupuesto valido')
+      }
+
+   }
 
    return (
       <div className='contenedor-presupuesto contenedor sombra' >
          
-         <form className='formulario'>
+         <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                <label>Definir Presupuesto</label>
 
@@ -19,6 +36,9 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                />
             </div>
             <input type='submit' value='Anadir' />
+
+            {!!mensaje && <Mensaje tipo='error'> {mensaje} </Mensaje>}
+
          </form>
       </div>
    )
