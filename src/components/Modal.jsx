b2de25b59f8190d8c0031b7eef350fdb56233d5e
antdev1987 <antdev1987@gmail.com>
2022-01-05 22:07:11 -0600

    103 anadiendo el formulario de gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -40,6 +40,46 @@ const Modal = ({ setModal, modal }) => {
 
                 <legend>Nuevo Gasto</legend>
 
+                <div className='campo'>
+                    <label htmlFor='nombre'>Nombre gasto</label>
+                    <input
+                        id='nombre'
+                        type='text'
+                        name='nombre'
+                        placeholder='Anade nombre de gasto'
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor='cantidad'>Cantidad</label>
+                    <input
+                        id='cantidad'
+                        type='number'
+                        name='cantidad'
+                        placeholder='Ingresa una canitdad'
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor='categoria'>Categoria</label>
+
+                    <select id='categoria'>
+                        <option value=''> -- Seleccione --</option>
+                        <option value='ahorro'>Ahorro</option>
+                        <option value='comida'>Comida</option>
+                        <option value='casa'>Casa</option>
+                        <option value='gastos'>Gastos Varios</option>
+                        <option value='ocio'>Ocio</option>
+                        <option value='salud'>Salud</option>
+                        <option value='suscripciones'>Suscripciones</option>
+                    </select>
+                </div>
+
+                <input 
+                    type='submit'
+                    value='Enviar Gasto'
+                />
+
             </form>
         </div>
     )
